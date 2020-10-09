c772c4874e167d0de95b0efd4b11ec38e29c0abd
Caroline <carolinechatelon@hotmail.fr>
2020-10-09 11:33:59 +0200

    add gsap animation

---
+++ app.js
@@ -0,0 +1,22 @@
+const titreSpans = document.querySelectorAll('h1 span');
+const btns= document.querySelectorAll('.btn-first');
+const logo = document.querySelectorAll('.logo');
+const medias= document.querySelectorAll('.bulle');
+const l1 = document.querySelectorAll('.l1');
+const l2 = document.querySelectorAll('.l2');
+
+window.addEventListener('load', ()=>{
+
+    const TL= gsap.timeline({paused:true});
+
+    TL
+    .staggerFrom(titreSpans, 1,{top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .staggerFrom(btns, 1,{ opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .from(l1, 1,{width:0, ease:"power2.out"}, '-=2')
+    .from(l2, 1,{width:0, ease:"power2.out"}, '-=2')
+    .from(logo, 1,{transform:"scale(0)", ease:"power2.out"}, '-=2')
+    .staggerFrom(medias, 1,{ right:-200, ease: "power2.out"}, 0.3, '-=1')
+
+
+    TL.play()
+})
